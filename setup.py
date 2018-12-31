3a631f70ebe309e8e02cae3534999f4883e8e18c
edwinwid <nedwinwid@gmail.com>
2018-12-31 22:23:46 +0700

    Preparing for packaging
    massage the directory structure to conform to standard python package
    dir tree

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lib_python',
+	  version='1.0.0',
+	  description='Collection of useful python & pandas libraries',
+	  url='https://gitlab.com/edwinwid/lib_python',
+	  author='N. Edwin Widjonarko',
+	  author_email='nedwinwid@gmail.com',
+	  license='BSD',
+	  packages=['lib_python'],
+	  zip_safe=False)
